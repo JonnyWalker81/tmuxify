736b76298d0710dea36c398f3ae202bb9adafef8
Jonathan Rothberg <jonnyrpi@gmail.com>
2020-02-19 21:06:44 -0800

    Initial Commit.

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::process::Command;
+
+fn main() -> Result<(), serde_yaml::Error> {
+    // println!("Hello, world!");
+
+    let yaml = std::fs::read_to_string("sample.yml").unwrap();
+
+    let yaml_map: serde_yaml::Value = serde_yaml::from_str(&yaml)?;
+
+    let session_name = match &yaml_map["name"] {
+        serde_yaml::Value::String(s) => s.clone(),
+        _ => String::from("default"),
+    };
+
+    // println!("Yaml: {:#?}", yaml_map["name"]);
+    // println!("Yaml: {:#?}", yaml_map["root"]);
+    // println!("Yaml: {:#?}", yaml_map["windows"]);
+    // println!("Yaml: {:#?}", yaml_map["windows"][0]["doom"]);
+
+    Command::new("tmux")
+        .args(&["new", "-d", "-s", &session_name])
+        .output()
+        .expect("Failed to execute process.");
+
+    let mut is_first_window = true;
+    match &yaml_map["windows"] {
+        serde_yaml::Value::Sequence(s) => {
+            for w in s {
+                match w {
+                    serde_yaml::Value::Mapping(m) => {
+                        for (k, v) in m.iter() {
+                            // println!("({:#?}, {:#?})", k, v);
+                            if !is_first_window {
+                                Command::new("tmux")
+                                    .args(&["new-window", "-t", &session_name])
+                                    .output()
+                                    .expect("Failed to execute process.");
+                            }
+
+                            is_first_window = false;
+                            Command::new("tmux")
+                                .args(&[
+                                    "rename-window",
+                                    "-t",
+                                    &session_name,
+                                    k.as_str().unwrap_or("shell"),
+                                ])
+                                .output()
+                                .expect("Failed to execute process.");
+                            match v {
+                                serde_yaml::Value::Mapping(inner) => {
+                                    if let Some(layout) =
+                                        inner.get(&serde_yaml::Value::String("layout".into()))
+                                    {
+                                        // println!("Has Layout...{:#?}", layout);
+                                        if let serde_yaml::Value::String(l) = layout {
+                                            match l.as_str() {
+                                                "main-horizontal" => {
+                                                    Command::new("tmux")
+                                                        .args(&[
+                                                            "split-window",
+                                                            "-t",
+                                                            &session_name,
+                                                        ])
+                                                        .output()
+                                                        .expect("Failed to execute process.");
+                                                }
+                                                "main-vertical" => {
+                                                    Command::new("tmux")
+                                                        .args(&[
+                                                            "split-window",
+                                                            "-v",
+                                                            "-t",
+                                                            &session_name,
+                                                        ])
+                                                        .output()
+                                                        .expect("Failed to execute process.");
+                                                }
+                                                _ => {}
+                                            }
+                                        }
+                                    }
+
+                                    if let Some(_panes) =
+                                        inner.get(&serde_yaml::Value::String("panes".into()))
+                                    {
+                                        // println!("Has Panes...{:#?}", panes);
+                                    }
+                                }
+
+                                _ => {}
+                            }
+                        }
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
+    }
+
+    // println!("Attaching...");
+    Command::new("tmux")
+        .args(&["attach-session", "-t", &session_name])
+        .status()
+        .expect("Failed to execute process.");
+
+    Ok(())
+}
